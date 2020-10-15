0497b0d3de1234278b72956e9809fa9e4e0afc30
KallDrexx <me@mshapiro.net>
2020-10-14 21:50:14 -0400

    Added simple command line utility for code generation

---
+++ Parme.Cli/CodeGenHandler.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.CommandLine;
+using System.CommandLine.IO;
+using System.IO;
+using Parme.Core;
+using Parme.Core.Serialization;
+using Parme.CSharp.CodeGen;
+
+namespace Parme.Cli
+{
+    public static class CodeGenHandler
+    {
+        public static void Handle(string inputFile, 
+            string outputFile, 
+            string language, 
+            string className, 
+            string @namespace, 
+            IConsole console)
+        {
+            if (!File.Exists(inputFile))
+            {
+                console.Error.WriteLine($"The file '{inputFile}' does not exist");
+                return;
+            }
+
+            if (string.IsNullOrWhiteSpace(outputFile))
+            {
+                console.Error.WriteLine("An output file is required");
+                return;
+            }
+
+            switch (language?.ToLower().Trim())
+            {
+                case "csharp":
+                    GenerateCSharpCode(inputFile, outputFile, className, @namespace, console);
+                    break;
+                
+                default:
+                    console.Error.WriteLine("Invalid language option.  Valid options: \"csharp\"");
+                    return;
+            }
+        }
+
+        private static void GenerateCSharpCode(string inputFile, string outputFile, string className, string @namespace, IConsole console)
+        {
+            console.Out.WriteLine($"Generating C# code for emitter logic file '{inputFile}' into '{outputFile}'");
+            
+            string json;
+            try
+            {
+                json = File.ReadAllText(inputFile);
+            }
+            catch (Exception exception)
+            {
+                console.Error.WriteLine($"Failed to read input file '{inputFile}': {exception.Message}");
+                return;
+            }
+
+            EmitterSettings emitterSettings;
+            try
+            {
+                emitterSettings = EmitterSettings.FromJson(json);
+            }
+            catch (MissingParmeTypeException exception)
+            {
+                var message = $"The emitter logic defined in '{inputFile}' could not be read, as it is expecting a " +
+                              $"ParME type of '{exception.TypeName}', but this type is not known.";
+
+                console.Error.WriteLine(message);
+
+                return;
+            }
+            catch (Exception exception)
+            {
+                var message = $"Failed to parse emitter logic defined in `{inputFile}`: {exception.Message}" +
+                              $"{Environment.NewLine}{Environment.NewLine}{exception}";
+
+                console.Error.WriteLine(message);
+                return;
+            }
+
+            @namespace ??= "Parme.Generated";
+            className ??= Path.GetFileNameWithoutExtension(inputFile);
+            var code = EmitterLogicClassGenerator.Generate(emitterSettings, @namespace, className, false);
+
+            try
+            {
+                File.WriteAllText(outputFile, code);
+            }
+            catch (Exception exception)
+            {
+                console.Error.WriteLine($"Failed to write generated code to '{outputFile}': {exception.Message}");
+                return;
+            }
+            
+            console.Out.WriteLine("Parme emitter logic code successfully generated");
+        }
+    }
+}
---
+++ Parme.Cli/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.CommandLine;
+using System.CommandLine.Invocation;
+
+namespace Parme.Cli
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var codeGenCommand = new Command("codegen", "Generate code for emitter logic")
+            {
+                new Argument<string>("inputFile", "Input emitter logic definition file"),
+                new Argument<string>("outputFile", "Name of the file to generate"),
+                new Argument<string>("language", "language for the generated output (valid values: \"csharp\")"),
+                new Option<string>("--className", "Name to give the generated class"),
+                new Option<string>("--namespace", "Namespace to put generated code in"),
+            };
+            
+            codeGenCommand.Handler = CommandHandler.Create<string, string, string, string, string, IConsole>(CodeGenHandler.Handle);
+            var commands = new RootCommand
+            {
+                codeGenCommand,
+            };
+
+            commands.Invoke(args);
+        }
+    }
+}
