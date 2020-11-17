e843eb817475904cb5514a4dc8c042ecf4f1ad65
KallDrexx <me@mshapiro.net>
2020-11-16 20:03:27 -0500

    Added modifier to set the rotation based on the particle's current velocity

---
+++ Parme.CSharp/CodeGen/Modifiers/VelocityBasedRotationCodeGen.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Parme.Core.Modifiers;
+
+namespace Parme.CSharp.CodeGen.Modifiers
+{
+    public class VelocityBasedRotationCodeGen : IGenerateCode
+    {
+        public Type ParmeObjectType => typeof(VelocityBasedRotationModifier);
+        
+        public FormattableString GenerateProperties(object obj)
+        {
+            return $"";
+        }
+
+        public FormattableString GenerateFields(object obj)
+        {
+            return $"";
+        }
+
+        public FormattableString GenerateExecutionCode(object obj)
+        {
+            return $"particle.RotationInRadians = (float) Math.Atan2(particle.Velocity.Y, particle.Velocity.X);";
+        }
+
+        public FormattableString GenerateCapacityEstimationCode(object obj)
+        {
+            return $"";
+        }
+    }
+}
---
+++ Parme.Editor/Ui/Elements/Editors/Modifiers/VelocityBasedRotationEditor.cs
@@ -0,0 +1,23 @@
+﻿using ImGuiNET;
+using Parme.Core.Modifiers;
+
+namespace Parme.Editor.Ui.Elements.Editors.Modifiers
+{
+    [EditorForType(typeof(VelocityBasedRotationModifier))]
+    public class VelocityBasedRotationEditor : SettingsEditorBase
+    {
+        protected override void CustomRender()
+        {
+            ImGui.TextWrapped("Every frame the particle will have the rotation set to be the same as the the " +
+                              "particle's velocity.\n\nThis modifier overrides all other rotational settings");
+        }
+
+        protected override void OnNewSettingsLoaded()
+        {
+        }
+
+        protected override void OnSelfManagedPropertyChanged(string propertyName)
+        {
+        }
+    }
+}
